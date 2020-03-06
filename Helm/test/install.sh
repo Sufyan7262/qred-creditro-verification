ebdffab64855074ae00a64371f7e6d87b9822be0
Sufyan7262 <59707212+Sufyan7262@users.noreply.github.com>
2020-03-06 10:52:04 +0100

    Deployment Changes

--- Helm/test/install.sh
+++ Helm/test/install.sh
@@ -9,7 +9,7 @@ cd ..
 kubectl config use-context arn:aws:eks:eu-west-1:308405705166:cluster/eks-test-03
 
 # Generate secrets
-./create-secrets-file.py --env test
+#./create-secrets-file.py --env test
 
 # Install service
 helm install --name example-service ./ -f test/values.yaml -f test/secrets.values.yaml
