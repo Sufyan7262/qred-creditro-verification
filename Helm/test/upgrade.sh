34e808c63ad3cddffd56818c6bb222123ac6d8d0
Sufyan7262 <59707212+Sufyan7262@users.noreply.github.com>
2020-03-06 10:43:14 +0100

    Deployment Changes

--- Helm/test/upgrade.sh
+++ Helm/test/upgrade.sh
@@ -9,6 +9,7 @@ cd ..
 kubectl config use-context arn:aws:eks:eu-west-1:308405705166:cluster/eks-test-03
 
 # Generate secrets
+./create-secrets-file.py --env test
 
 # Upgrade service
 helm upgrade example-service ./ -f test/values.yaml -f test/secrets.values.yaml
